d3524be87b695f88ed9244d74c03f134b3c9a3f4
Blastmaster Fixup & Kleanup Crew <191918158+Blastmaster-Fixup-and-Kleanup-Crew@users.noreply.github.com>
2026-09-17 15:45:57 -0400

    Add OnDrawingToolbarButtonClick event handler

--- MainWindow.xaml.h
+++ MainWindow.xaml.h
@@ -26,6 +26,7 @@ namespace winrt::WinUI3TextEditor::implementation
         void OnHelpItemClick(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
         void OnStandardToolbarButtonClick(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
         void OnZoomComboBoxChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::SelectionChangedEventArgs const& e);
+        void OnDrawingToolbarButtonClick(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
     };
 }
 
